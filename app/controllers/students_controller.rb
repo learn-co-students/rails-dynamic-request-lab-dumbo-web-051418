0ac44267e3beb929d1b91b37a8aedb47ed631221
github username <github email address>
2018-06-08 15:00:00 -0400

    implemented individual student pages

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,9 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show_specific
+    @student = Student.find(params[:id])
+    render :'show.html'
+  end
 end

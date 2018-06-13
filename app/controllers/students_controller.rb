022a3072135033ccc79720fc8f15d4d45d8b8073
github username <github email address>
2018-06-13 15:25:13 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,100 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: :show
+
   def index
     @students = Student.all
   end
+
+  def show
+  end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class StudentsController < ApplicationController
+#
+#   # do render:
+#
+#   def index
+#     # show all the students
+#     @students = Student.all
+#     render :index
+#   end
+#
+#   def show
+#     # show a single student
+#   end
+#
+#   def new
+#     # show a form to make a new student
+#   end
+#
+#   def edit
+#     # show a form to edit a student
+#   end
+#
+#   # don't render:
+#
+#   def create
+#     # use form submission to create a student, redirect to show or index
+#     student = Student.create(some_params)
+#     redirect_to :student_url(@student)
+#   end
+#
+#   def update
+#     # use form submission to update a student, redirect to show or index
+#
+#   end
+#
+#   def delete
+#     # delete a student. redirect to index.
+#
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  resources :students, only: [:index, :show, :new, :edit, :create, :update, :delete]
+
+  # get '/students', to: 'students#index'
+  # get '/students/new', to: 'students#new'
+  # get '/students/:id', to: 'students#show'
+  # get '/students/:id/edit', to: 'students#edit'
+  #
+  # post '/students', to: 'students#create'
+  # patch '/students/:id', to: 'students#update'
+  # delete '/students/:id', to: 'students#delete'
+
+
 end
